0c3c0682f5245d98cc79b5ac4bb6547a08105224
Ronen Amiel <ronen.amiel@gmail.com>
2016-10-27 16:46:01 +0300

    removing clean command

--- wix-node-build.js
+++ wix-node-build.js
@@ -7,7 +7,6 @@ program
   .command('start [entryPoint]', 'start the application')
   .command('release', 'bump package.json')
   .command('build', 'build the application')
-  .command('clean', 'remove unnecessary filder/folders')
   .command('test', 'test the application')
   .command('lint', 'lints the code')
   .parse(process.argv);
