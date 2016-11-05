4e0c06ccbc7bf1e13497fe836183cab768b89ace
Shahar Talmi <shahart@wix.com>
2016-11-06 00:53:04 +0200

    refactorign start

--- wix-node-build-start.js
+++ wix-node-build-start.js
@@ -4,7 +4,6 @@ const gulp = require('gulp');
 const plugins = require('gulp-load-plugins')();
 const program = require('commander');
 const suffix = require('./lib/utils').suffix;
-const start = require('./lib/tasks/aggregators/start');
 
 program
   .option('-e, --entry-point <entry>', 'entry point of the application', suffix('.js'), 'index')
@@ -14,14 +13,4 @@ program
   .option('-h, --hot', 'use hot module replacement')
   .parse(process.argv);
 
-const loadTasks = options => {
-  require('./lib/tasks/node')(gulp, plugins, options);
-  require('./lib/tasks/babel')(gulp, plugins, options);
-  require('./lib/tasks/typescript')(gulp, plugins, options);
-  require('./lib/tasks/sass')(gulp, plugins, options);
-  require('./lib/tasks/mocha')(gulp, plugins, options);
-  require('./lib/tasks/copy-assets')(gulp, plugins, options);
-  require('./lib/tasks/clean')(gulp, plugins, options);
-};
-
-start(program, loadTasks);
+require('./lib/tasks/aggregators/start')(gulp, plugins, program);
