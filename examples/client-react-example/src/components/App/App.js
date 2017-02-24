40958ddced1b40dee24082a43d3a9e06a8f8dbfc
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-02-25 00:20:03 +0200

    example - client react (#1)

---
+++ examples/client-react-example/src/components/App/App.js
@@ -0,0 +1,22 @@
+import React, {PropTypes} from 'react';
+import {translate} from 'react-i18next';
+import s from './App.scss';
+
+function App({t}) {
+  return (
+    <div className={s.root}>
+      <div className={s.header}>
+        <h2>{t('app.title')}</h2>
+      </div>
+      <p className={s.intro}>
+        {t('app.intro')}
+      </p>
+    </div>
+  );
+}
+
+App.propTypes = {
+  t: PropTypes.func
+};
+
+export default translate(null, {wait: true})(App);
