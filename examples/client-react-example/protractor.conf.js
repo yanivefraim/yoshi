7030c60375e06a2c3c4d62b3bb0cf95b677e823b
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-02-25 00:27:27 +0200

    update examples

--- examples/client-react-example/protractor.conf.js
+++
@@ -1,9 +0,0 @@
-import {start} from './test/mock/fake-server';
-
-export const config = {
-  baseUrl: 'http://localhost:3100/',
-  onPrepare() {
-    browser.ignoreSynchronization = true;
-    start(3100);
-  }
-};
--- examples/client-react-example/test/e2e/app.e2e.js
+++
@@ -1,8 +0,0 @@
-describe('React application', () => {
-  describe('open page', () => {
-    it('should display title', () => {
-      browser.get('/');
-      expect($('h2').getText()).toBe('Hello World!');
-    });
-  });
-});
